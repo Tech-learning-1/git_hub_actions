13ef3fde974d00a3177ee45253c4cef989e134e2
santhosh varal <santhoshvarala@example.com>
2026-02-14 19:03:47 +0530

    modified action file

--- shell.py
+++ shell.py
@@ -8,6 +8,8 @@ file = Path("action1.yaml")
 directory = Path("src")
 current_dir = Path.cwd()
 present_dir = os.getcwd()
+print(f"Current directory using pathlib: {current_dir}")
+print(f"Current directory using os: {present_dir}")
 def deletefile(file):
     if file.exists():
         file.unlink()
