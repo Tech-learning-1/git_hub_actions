96f211fafa3de02e3bdb2f286c2a8691149aeff2
santhosh varal <santhoshvarala@example.com>
2026-02-14 18:28:58 +0530

    added calc python file along with scritping

---
+++ calc.py
@@ -0,0 +1,5 @@
+def add(a, b):
+    return a + b
+def unit_test():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
---
+++ shell.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+import shutil
+source = Path('calc.py')
+dest = Path('src/calc.py')
+file = Path("action1.yaml")
+directory = Path("src")
+
+def deletefile(file):
+    if file.exists():
+          file.unlink()
+    else:
+        print(f"File {file} does not exist.")
+
+
+def makedirectory(directory):
+      directory.mkdir(exist_ok=True)
+
+
+
+def  movefile(source, dest):
+       if source.exists():
+           shutil.move(str(source), str(dest))
+       else:
+              print(f"Source file {source} does not exist.")
+              
+makedirectory(directory)
+movefile(source, dest)
+deletefile(file)
