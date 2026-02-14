63cfb3278e560ed832e9bb8e2f2ab2ca28cc0587
santhosh varal <santhoshvarala@example.com>
2026-02-14 18:57:48 +0530

    modified action file

--- shell.py
+++ shell.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import shutil
+
 source = Path('calc.py')
 dest = Path('src/calc.py')
 file = Path("action1.yaml")
@@ -7,22 +8,19 @@ directory = Path("src")
 
 def deletefile(file):
     if file.exists():
-          file.unlink()
+        file.unlink()
     else:
         print(f"File {file} does not exist.")
 
-
 def makedirectory(directory):
-      directory.mkdir(exist_ok=True)
+    directory.mkdir(exist_ok=True)
 
+def movefile(source, dest):  # Fixed indentation
+    if source.exists():
+        shutil.move(str(source), str(dest))
+    else:
+        print(f"Source file {source} does not exist.")
 
-
-def  movefile(source, dest):
-       if source.exists():
-           shutil.move(str(source), str(dest))
-       else:
-              print(f"Source file {source} does not exist.")
-              
 makedirectory(directory)
 movefile(source, dest)
 deletefile(file)
